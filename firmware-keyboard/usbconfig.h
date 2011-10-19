1a7d7f6a90c6279512882cd0dbe57f2d2297fce9
Fabio Baltieri <fabio.baltieri@gmail.com>
2011-10-20 00:50:44 +0200

    usbconfig: remove vendor and product strings to save space

--- firmware-keyboard/usbconfig.h
+++ firmware-keyboard/usbconfig.h
@@ -242,8 +242,10 @@ extern void hadUsbReset(void);
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
+/*
 #define USB_CFG_VENDOR_NAME     'o', 'b', 'd', 'e', 'v', '.', 'a', 't'
 #define USB_CFG_VENDOR_NAME_LEN 8
+*/
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -252,8 +254,10 @@ extern void hadUsbReset(void);
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
+/*
 #define USB_CFG_DEVICE_NAME     'H', 'I', 'D', 'K', 'e', 'y', 's'
 #define USB_CFG_DEVICE_NAME_LEN 7
+*/
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
