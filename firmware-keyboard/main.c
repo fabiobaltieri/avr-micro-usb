f33fef98fb3e20d7d75422af6bb3d5c896e37ef6
Fabio Baltieri <fabio.baltieri@gmail.com>
2011-10-20 00:51:33 +0200

    main: initial attiny25 support

--- firmware-keyboard/main.c
+++ firmware-keyboard/main.c
@@ -8,11 +8,10 @@
  * This Revision: $Id: main.c 299 2007-03-29 17:07:19Z cs $
  */
 
-#define F_CPU   12000000L    /* evaluation board runs on 4MHz */
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
+#include <avr/eeprom.h>
 #include <avr/wdt.h>
 #include <string.h>
 
@@ -226,12 +225,79 @@ uchar	usbFunctionSetup(uchar data[8])
 }
 
 /* ------------------------------------------------------------------------- */
+/* ------------------------ Oscillator Calibration ------------------------- */
+/* ------------------------------------------------------------------------- */
+
+/* Calibrate the RC oscillator to 8.25 MHz. The core clock of 16.5 MHz is
+ * derived from the 66 MHz peripheral clock by dividing. Our timing reference
+ * is the Start Of Frame signal (a single SE0 bit) available immediately after
+ * a USB RESET. We first do a binary search for the OSCCAL value and then
+ * optimize this value with a neighboorhod search.
+ * This algorithm may also be used to calibrate the RC oscillator directly to
+ * 12 MHz (no PLL involved, can therefore be used on almost ALL AVRs), but this
+ * is wide outside the spec for the OSCCAL value and the required precision for
+ * the 12 MHz clock! Use the RC oscillator calibrated to 12 MHz for
+ * experimental purposes only!
+ */
+static void calibrateOscillator(void)
+{
+uchar       step = 128;
+uchar       trialValue = 0, optimumValue;
+int         x, optimumDev, targetValue = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
+
+    /* do a binary search: */
+    do{
+        OSCCAL = trialValue + step;
+        x = usbMeasureFrameLength();    /* proportional to current real frequency */
+        if(x < targetValue)             /* frequency still too low */
+            trialValue += step;
+        step >>= 1;
+    }while(step > 0);
+    /* We have a precision of +/- 1 for optimum OSCCAL here */
+    /* now do a neighborhood search for optimum value */
+    optimumValue = trialValue;
+    optimumDev = x; /* this is certainly far away from optimum */
+    for(OSCCAL = trialValue - 1; OSCCAL <= trialValue + 1; OSCCAL++){
+        x = usbMeasureFrameLength() - targetValue;
+        if(x < 0)
+            x = -x;
+        if(x < optimumDev){
+            optimumDev = x;
+            optimumValue = OSCCAL;
+        }
+    }
+    OSCCAL = optimumValue;
+}
+/*
+Note: This calibration algorithm may try OSCCAL values of up to 192 even if
+the optimum value is far below 192. It may therefore exceed the allowed clock
+frequency of the CPU in low voltage designs!
+You may replace this search algorithm with any other algorithm you like if
+you have additional constraints such as a maximum CPU clock.
+For version 5.x RC oscillators (those with a split range of 2x128 steps, e.g.
+ATTiny25, ATTiny45, ATTiny85), it may be useful to search for the optimum in
+both regions.
+*/
+
+void    hadUsbReset(void)
+{
+    calibrateOscillator();
+    eeprom_write_byte(0, OSCCAL);   /* store the calibrated value in EEPROM */
+}
+
+/* ------------------------------------------------------------------------- */
 
 int	main(void)
 {
   uchar   keyDidChange = 1;
   uchar   idleCounter = 0;
   uint16_t key, lastKey = 0;
+  uchar   calibrationValue;
+
+  calibrationValue = eeprom_read_byte(0); /* calibration value from last time */
+  if(calibrationValue != 0xff){
+    OSCCAL = calibrationValue;
+  }
 
   CLKPR = _BV(CLKPCE);
   CLKPR = 0x00;
@@ -256,8 +322,8 @@ int	main(void)
       keyDidChange = 1;
     }
 
-    if(TIFR0 & (1<<TOV0)){   /* 22 ms timer */
-      TIFR0 = 1<<TOV0;
+    if(TIFR & (1<<TOV0)){   /* 22 ms timer */
+      TIFR = 1<<TOV0;
       if(idleRate != 0){
 	if(idleCounter > 4){
 	  idleCounter -= 5;   /* 22 ms in units of 4 ms */
